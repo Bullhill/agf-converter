7426c0cf6a89bd886bb0813d4f4254b3ac1c4f3e
Ole Andreas <Ole1000000@hotmail.com>
2023-04-23 23:04:26 +0200

    Add project files.

---
+++ agf converter/FieldParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agf_converter
+{
+    public class FieldParser
+    {
+        public static void ParseGeometry()
+        {
+
+        }
+    }
+}
