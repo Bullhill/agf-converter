73d735aa9af770394ac285da0f940e8d07825917
Ole Andreas Oksås <Ole.Andreas.Oksas@akersolutions.com>
2023-04-24 07:45:40 +0200

    Unzip og dcrypt

--- agf converter/Program.cs
+++ agf converter/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System.Xml;
 using System.Security.Cryptography;
 
-string folder = "C:\\temp\\Fields";
+string folder = "C:\\Users\\169646\\source\\repos\\Bullhill\\agf-converter\\agf converter\\Fields";
 
 List<string> agfFiles = Directory.GetFiles(folder, "*.agf").ToList();
 
-foreach(string file in agfFiles)
+foreach (string file in agfFiles)
 {
     FileInfo fi = new FileInfo(file);
     string FileFirstName = fi.Name.Replace(".agf", "");
@@ -52,94 +52,97 @@ foreach(string file in agfFiles)
         {
             using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
             {
-                using (StreamReader sr = new StreamReader(cs))
+                var decompressor = new System.IO.Compression.GZipStream(cs, System.IO.Compression.CompressionMode.Decompress);
+                FileStream outputFileStream = File.Create(extractFolder + "\\" + FileFirstName + ".xml");
+                decompressor.CopyTo(outputFileStream);
+                outputFileStream.Close();
+            }
+        }
+
+    }
+
+
+
+
+
+    string encodedString = String.Empty;
+
+    XmlDocument doc = new XmlDocument();
+    try
+    {
+        doc.Load(extractFolder + "\\" + FileFirstName + ".xml");
+        XmlNode? boundaries = doc.GetElementsByTagName("field")[0];
+        if (boundaries != null)
+        {
+            XmlNode? boundariesNode = boundaries["boundaries"];
+            if (boundariesNode != null)
+            {
+                foreach (XmlNode boundary in boundariesNode.ChildNodes)
                 {
-                    File.WriteAllText(extractFolder + "\\" + FileFirstName + ".xml.gz", sr.ReadToEnd());
+                    XmlNode? geometry = boundary["geometry"];
+                    if (geometry != null)
+                        encodedString = geometry.InnerText;
                 }
             }
         }
-
     }
-}
-
-
-
-
-string encodedString = String.Empty;
-
-XmlDocument doc = new XmlDocument();
-try
-{
-    doc.Load("c:\\temp\\0f23d941-f5db-a43a-9c91-913781046f91.xml");
-    XmlNode? boundaries = doc.GetElementsByTagName("field")[0];
-    if(boundaries != null)
+    catch
     {
-        XmlNode? boundariesNode = boundaries["boundaries"];
-        if(boundariesNode != null)
-        {
-            foreach(XmlNode boundary in boundariesNode.ChildNodes)
-            {
-                XmlNode? geometry = boundary["geometry"];
-                if (geometry != null)
-                    encodedString = geometry.InnerText;
-            }
-        }
+
     }
-}
-catch
-{
+
+    byte[] data = Convert.FromBase64String(encodedString);
+    List<string> coordinates = new List<string>();
+
+    // 0 == Point, 1 == Line, 3 == Polygon, 4 == MultiPolygon
+
+    //Skip first 5 bytes
+    int i = 5;
+
+    while (i + 24 <= data.Length)
+    {
+
+        Double x = BitConverter.ToDouble(data, i + 0 * 8);
+        Double y = BitConverter.ToDouble(data, i + 1 * 8);
+        Double z = BitConverter.ToDouble(data, i + 2 * 8);
+
+
+        CoordinateSharp.Coordinate loc = CoordinateSharp.ECEF.ECEFToLatLong(x / 1000, y / 1000, z / 1000);
+
+        coordinates.Add(loc.Longitude.DecimalDegree + "," + loc.Latitude.DecimalDegree + ",0");
+
+        i += 24;
+    }
+
+
+    using (XmlWriter writer = XmlWriter.Create(extractFolder + "\\" + FileFirstName + ".kml", new XmlWriterSettings() { Indent = true, IndentChars = "  " }))
+    {
+        writer.WriteStartDocument();
+        writer.WriteStartElement("", "kml", "http://www.opengis.net/kml/2.2");
+        writer.WriteStartElement("Placemark");
+        writer.WriteElementString("name", "Hasseljordet Midt");
+        writer.WriteStartElement("Polygon");
+        writer.WriteStartElement("outerBoundaryIs");
+        writer.WriteStartElement("LinearRing");
+        writer.WriteElementString("coordinates", String.Join(" ", coordinates));
+        writer.WriteEndElement();
+        writer.WriteEndElement();
+        writer.WriteEndElement();
+        writer.WriteEndElement();
+        writer.WriteEndElement();
+        writer.WriteEndDocument();
+
+    }
+
 
 }
-
-byte[] data = Convert.FromBase64String(encodedString);
-List<string> coordinates = new List<string>();
-
-// 0 == Point, 1 == Line, 3 == Polygon, 4 == MultiPolygon
-
-//Skip first 5 bytes
-int i = 5;
-
-while (i+24 <= data.Length)
-{
-    
-    Double x = BitConverter.ToDouble(data, i + 0 * 8);
-    Double y = BitConverter.ToDouble(data, i + 1 * 8);
-    Double z = BitConverter.ToDouble(data, i + 2 * 8);
-
-
-    CoordinateSharp.Coordinate loc = CoordinateSharp.ECEF.ECEFToLatLong(x / 1000, y / 1000, z / 1000);
-
-    coordinates.Add(loc.Longitude.DecimalDegree + "," + loc.Latitude.DecimalDegree + ",0");
-
-    i += 24;
-}
-
-
-using (XmlWriter writer = XmlWriter.Create("test2.kml", new XmlWriterSettings() { Indent = true, IndentChars = "  "}))
-{
-    writer.WriteStartDocument();
-    writer.WriteStartElement("", "kml", "http://www.opengis.net/kml/2.2");
-    writer.WriteStartElement("Placemark");
-    writer.WriteElementString("name", "Hasseljordet Midt");
-    writer.WriteStartElement("Polygon");
-    writer.WriteStartElement("outerBoundaryIs");
-    writer.WriteStartElement("LinearRing");
-    writer.WriteElementString("coordinates", String.Join(" ", coordinates));
-    writer.WriteEndElement();
-    writer.WriteEndElement();
-    writer.WriteEndElement();
-    writer.WriteEndElement();
-    writer.WriteEndElement();
-    writer.WriteEndDocument();
-
-}
-
 
 
 static byte[] StringToByteArray(string hex)
 {
     return Enumerable.Range(0, hex.Length)
-                     .Select(x => Convert.ToByte(hex.Substring(x, 1), 16))
+                     .Where(x => x % 2 == 0)
+                     .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
                      .ToArray();
 }
 
